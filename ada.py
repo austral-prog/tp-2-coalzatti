c2caf7de3141384230d5f971d336179af4d67fec
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-03-25 21:20:22 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,8 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    print(first_name.lower()+" "+last_name.lower())
-    print(first_name.title()+" "+last_name.title())
-    print(first_name.upper()+" "+last_name.upper())
+    print("\n"+first_name.lower()+" "+last_name.lower())
+    print("\n"+first_name.title()+" "+last_name.title())
+    print("\n"+first_name.upper()+" "+last_name.upper())
     print("\t"+first_name.lower()+" "+last_name.lower())
 ada()
+
+
